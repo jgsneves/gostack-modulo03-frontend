bf5aca6e9fef3d75cca8bf0cbeb2bd6bdb843131
joao gabriel neves <jgsn.adv@gmail.com>
2021-03-30 23:04:09 -0300

    dashboard style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import logoImg from '../../assets/logo.svg';
+import { Form, Title, Repositories } from './styles';
+import {FiChevronRight} from 'react-icons/fi';
 
 export const Dashboard: React.FC = () => {
     return (
-        <h1>Dashboard</h1>
+        <>
+            <img src={logoImg} alt="Github Explorer"/>
+            <Title>Explore repositórios no Github</Title>
+            <Form>
+                <input type="text" placeholder="Digite o nome do repositório"/>
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="/blabla">
+                    <div>
+                        <strong>João Gabriel</strong>
+                        <p>Repo bla bla</p>
+                    </div>
+                <FiChevronRight size={20}/>
+                </a>
+            </Repositories>
+        </>
     );
 }
